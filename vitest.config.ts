2e6521a29c3282bd689d63699701889cfbb3a8a4
peccu <peccul@gmail.com>
2023-12-13 23:04:15 +0900

    update from template

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,17 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      outputFile: {
+        html: './vitest-result/index.html',
+      },
+      reporters: ['default', 'html'],
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      exclude: [...configDefaults.exclude, 'e2e/*', 'vue-app/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        reportsDirectory: './vitest-result/coverage',
+      },
     }
   })
 )
