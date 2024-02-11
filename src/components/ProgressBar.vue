36b8bdf92969058be465b84c6e7c6fa18584d7c7
peccu <peccul+github@gmail.com>
2024-02-11 01:38:33 +0000

    fix progress bar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="w-full flex flex-col items-center">
     <!-- 予定の進捗バー -->
-    <div class="w-8 h-3/4 bg-gray-200 rounded-full mb-4 relative">
-      <div
-        :style="{ height: plannedProgress + '%' }"
-        class="absolute bottom-0 left-0 right-0 rounded-full bg-blue-500"
-      ></div>
+    <div class="w-full h-8 bg-gray-200 rounded-full">
+      <div :style="{ width: plannedProgress + '%' }" class="h-full rounded-full bg-blue-500"></div>
     </div>
     <!-- 実績の進捗バー -->
-    <div class="w-8 h-3/4 bg-gray-200 rounded-full relative">
-      <div
-        :style="{ height: actualProgress + '%' }"
-        class="absolute bottom-0 left-0 right-0 rounded-full bg-green-500"
-      ></div>
+    <div class="w-full h-8 bg-gray-200 rounded-full">
+      <div :style="{ width: actualProgress + '%' }" class="h-full rounded-full bg-green-500"></div>
     </div>
   </div>
 </template>
